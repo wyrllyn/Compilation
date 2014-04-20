6cdf1b677a50d07d683e73013720a5822418cbce
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-20 16:45:45 +0200

    added a bunch of new test files

---
+++ test_dup_both.pas
@@ -0,0 +1,37 @@
+program test;
+
+
+{function f}
+function f(e,y : boolean, z: integer) : integer;
+var i, e : integer;
+var j : integer;
+begin
+	i := 0;
+	f := 0;
+	while i < e do
+	begin
+		if i mod 2 = 0 then
+			f := f + i;
+			i := i + 1;
+	end;
+end;
+
+function a(b : char, arg : integer) : integer;
+var z2 : boolean;
+begin
+	z2 := true;
+	a := z2;
+end;
+
+procedure poney(corne, sabots : integer);
+var licorne : integer;
+begin
+	licorne := 3;
+end;
+
+{main program}
+var x : char;
+begin
+	readln(x);
+	writeln(f(x));
+end.
