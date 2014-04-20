d5ce3dc6b76fbd1e51565936150f678d01795c43
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-20 17:22:27 +0200

    nu test file

---
+++ test_dup_f_main.pas
@@ -0,0 +1,38 @@
+program test;
+
+
+{function f}
+function f(e,y : boolean, z: integer) : integer;
+var i, h : integer;
+var j : integer;
+begin
+	i := 0;
+	f := 0;
+	while i < e do
+	begin
+		if i mod 2 = 0 then
+			f := f + i;
+			i := i + 1;
+	end;
+end;
+
+function a(b : char, arg : integer) : integer;
+var z2 : boolean;
+begin
+	z2 := true;
+	a := z2;
+end;
+
+procedure poney(corne, sabots : integer);
+var licorne : integer;
+begin
+	licorne := 3;
+end;
+
+{main program}
+var x : char;
+var e : boolean;
+begin
+	readln(x);
+	writeln(f(x));
+end.
